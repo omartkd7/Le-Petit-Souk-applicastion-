0fca0d60d028241e4c0081d3e59f9257be45c247
omartkd7 <omartkd7@gmail.com>
2026-05-21 19:30:10 +0100

    im creeted file maine righht now

---
+++ src/app/page/Mine.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+
+function Mine() {
+  return (
+    <view>
+      <view>
+        <Text>Mine page</Text>
+      </view>
+    </view>
+  );
+}
+
+export default Mine;
