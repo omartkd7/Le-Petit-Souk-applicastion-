641c3de901dc29b4145a7b8112e2918f08253b9c
omartkd7 <omartkd7@gmail.com>
2026-05-21 20:17:59 +0100

    this last update

--- src/app/page/Mine.tsx
+++ src/app/page/Mine.tsx
@@ -1,10 +1,41 @@
-import { Text } from "react-native";
+import { Image, Text } from "react-native";
 
 function Mine() {
   return (
     <view>
       <view>
         <Text>Mine page</Text>
+      </view>
+
+      <view id="ithm prodact">
+        <view id="prodact 1">
+          <Text>prodact 1</Text>
+          <Image src=""></Image>
+        </view>
+
+        <view id="prodact 2">
+          <Text>prodact 1</Text>
+
+          <Image src=""></Image>
+        </view>
+
+        <view id="prodact 3">
+          <Text>prodact 1</Text>
+
+          <Image src=""></Image>
+        </view>
+
+        <view id="prodact 4">
+          <Text>prodact 1</Text>
+
+          <Image src=""></Image>
+        </view>
+
+        <view id="prodact 5">
+          <Text>prodact 1</Text>
+
+          <Image src=""></Image>
+        </view>
       </view>
     </view>
   );
