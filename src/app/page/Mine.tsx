30b974e9ecb8c987cb2fbb41e718249f88ce2121
omartkd7 <omartkd7@gmail.com>
2026-05-21 20:34:13 +0100

    in thid commit im add image of script

--- src/app/page/Mine.tsx
+++ src/app/page/Mine.tsx
@@ -10,31 +10,31 @@ function Mine() {
       <view id="ithm prodact">
         <view id="prodact 1">
           <Text>prodact 1</Text>
-          <Image src=""></Image>
+          <Image source={require("@/assets/IMAGE FOOD MAR/1.png")}></Image>
         </view>
 
         <view id="prodact 2">
           <Text>prodact 1</Text>
 
-          <Image src=""></Image>
+          <Image source={require("@/assets/IMAGE FOOD MAR/2.png")}></Image>
         </view>
 
         <view id="prodact 3">
           <Text>prodact 1</Text>
 
-          <Image src=""></Image>
+          <Image source={require("@/assets/IMAGE FOOD MAR/3.png")}></Image>
         </view>
 
         <view id="prodact 4">
           <Text>prodact 1</Text>
 
-          <Image src=""></Image>
+          <Image source={require("@/assets/IMAGE FOOD MAR/4.png")}></Image>
         </view>
 
         <view id="prodact 5">
           <Text>prodact 1</Text>
 
-          <Image src=""></Image>
+          <Image source={require("@/assets/IMAGE FOOD MAR/5.png")}></Image>
         </view>
       </view>
     </view>
