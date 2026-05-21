f1f1bac3adee9d40301be03cf685a462631032b3
omartkd7 <omartkd7@gmail.com>
2026-05-21 13:09:52 +0100

    this is next update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
---
+++ src/app/page/Mine.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@react-navigation/elements";
+
+export default function Home() {
+  return (
+    <view>
+      <Text>Home Page</Text>
+    </view>
+  );
+}
