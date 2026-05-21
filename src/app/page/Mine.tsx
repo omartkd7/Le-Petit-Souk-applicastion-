96920eebc5c7b4ac2d0a93ec7795c5102c1899da
omartkd7 <omartkd7@gmail.com>
2026-05-21 21:49:52 +0100

    this ferst commit

--- src/app/page/Mine.tsx
+++ src/app/page/Mine.tsx
@@ -1,4 +1,5 @@
 import { Image, Text } from "react-native";
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 
 function Mine() {
   return (
@@ -7,36 +8,38 @@ function Mine() {
         <Text>Mine page</Text>
       </view>
 
-      <view id="ithm prodact">
-        <view id="prodact 1">
-          <Text>prodact 1</Text>
-          <Image source={require("@/assets/IMAGE FOOD MAR/1.png")}></Image>
+      <ScrollView>
+        <view id="ithm prodact">
+          <view id="prodact 1">
+            <Text>prodact 1</Text>
+            <Image source={require("@/assets/IMAGE FOOD MAR/1.png")}></Image>
+          </view>
+
+          <view id="prodact 2">
+            <Text>prodact 1</Text>
+
+            <Image source={require("@/assets/IMAGE FOOD MAR/2.png")}></Image>
+          </view>
+
+          <view id="prodact 3">
+            <Text>prodact 1</Text>
+
+            <Image source={require("@/assets/IMAGE FOOD MAR/3.png")}></Image>
+          </view>
+
+          <view id="prodact 4">
+            <Text>prodact 1</Text>
+
+            <Image source={require("@/assets/IMAGE FOOD MAR/4.png")}></Image>
+          </view>
+
+          <view id="prodact 5">
+            <Text>prodact 1</Text>
+
+            <Image source={require("@/assets/IMAGE FOOD MAR/5.png")}></Image>
+          </view>
         </view>
-
-        <view id="prodact 2">
-          <Text>prodact 1</Text>
-
-          <Image source={require("@/assets/IMAGE FOOD MAR/2.png")}></Image>
-        </view>
-
-        <view id="prodact 3">
-          <Text>prodact 1</Text>
-
-          <Image source={require("@/assets/IMAGE FOOD MAR/3.png")}></Image>
-        </view>
-
-        <view id="prodact 4">
-          <Text>prodact 1</Text>
-
-          <Image source={require("@/assets/IMAGE FOOD MAR/4.png")}></Image>
-        </view>
-
-        <view id="prodact 5">
-          <Text>prodact 1</Text>
-
-          <Image source={require("@/assets/IMAGE FOOD MAR/5.png")}></Image>
-        </view>
-      </view>
+      </ScrollView>
     </view>
   );
 }
